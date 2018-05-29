403a5761853ac002b6c426d5f73d49bf17e5a57b
Tom A. Vibeto <tomandre@exorlive.com>
2018-05-29 14:58:11 +0200

    Reorganized code

--- src/Application.ts
+++ src/Application.ts
@@ -1,28 +1,62 @@
 import * as THREE from 'three';
 
 export default class Application {
-	constructor(el: HTMLElement) {
+	private scene = new THREE.Scene;
+	private camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	private renderer = new THREE.WebGLRenderer();
+	private sceneHandler = new SceneHandler(this.scene);
 
-		const scene = new THREE.Scene;
-		const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-		var renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		el.appendChild( renderer.domElement );
+	constructor(el: HTMLElement) {
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		el.appendChild(this.renderer.domElement);
+
+		this.camera.position.z = 5;
+		const ent = new MyCube();
 		
-		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-		var cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
+		this.sceneHandler.add(ent);
 		
-		camera.position.z = 5;
-		function animate() {
-			cube.rotation.x += 0.1;
-			cube.rotation.y += 0.1;
-			
-			requestAnimationFrame( animate );
-			renderer.render( scene, camera );
+		this.animate();
+	}
+	private animate() {
+		this.sceneHandler.tick();
+		requestAnimationFrame(() => { this.animate(); });
+		this.renderer.render(this.scene, this.camera);
+	}
+}
+class SceneHandler {
+	private scene: THREE.Scene;
+	private entities: Entity[] = [];
+	constructor(scene: THREE.Scene) {
+		this.scene = scene;
+	}
+	public add(entity: Entity) {
+		this.entities.push(entity);
+		this.scene.add(entity.getMesh());
+	}
+	public tick() {
+		for (var entity of this.entities) {
+			if (entity.tick) {
+				entity.tick();
+			}
 		}
-		animate();
-			
+	}
+}
+interface Entity {
+	tick(): void;
+	getMesh(): THREE.Mesh;
+}
+class MyCube implements Entity {
+	private mesh: THREE.Mesh;
+	constructor() {
+		var geometry = new THREE.BoxGeometry(1, 1, 1);
+		var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		this.mesh = new THREE.Mesh(geometry, material);
+	}
+	public getMesh(): THREE.Mesh {
+		return this.mesh;
+	}
+	public tick() {
+		this.mesh.rotation.x += 0.1;
+		this.mesh.rotation.y += 0.1;
 	}
 }
