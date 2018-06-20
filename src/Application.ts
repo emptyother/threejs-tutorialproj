32b6fd179b589e0324e7c8f8a318e49a1a0d7be6
Tom A. Vibeto <tom@onlyhuman.dk>
2018-06-20 04:37:16 +0200

    Drawing lines

--- src/Application.ts
+++ src/Application.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 export default class Application {
 	private scene = new THREE.Scene;
-	private camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	private camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
 	private renderer = new THREE.WebGLRenderer();
 	private sceneHandler = new SceneHandler(this.scene);
 
@@ -10,12 +10,18 @@ export default class Application {
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 		el.appendChild(this.renderer.domElement);
 
-		this.camera.position.z = 4;
 		const ent = new MyCube();
-		this.camera.lookAt(ent.getMesh().position);
+		//this.camera.lookAt(ent.getRenderObject().position);
+		this.camera.position.set(0,0,30);
+		this.camera.lookAt(new THREE.Vector3(0,0,0));
 		
 		this.sceneHandler.add(ent);
+		
 		//TODO: https://threejs.org/docs/index.html#manual/introduction/Drawing-lines
+		const lineob = new MyLines();
+		this.sceneHandler.add(lineob);
+		this.camera.lookAt(lineob.getRenderObject().position);
+		
 		this.animate();
 	}
 	private animate() {
@@ -32,7 +38,7 @@ class SceneHandler {
 	}
 	public add(entity: Entity) {
 		this.entities.push(entity);
-		this.scene.add(entity.getMesh());
+		this.scene.add(entity.getRenderObject());
 	}
 	public tick() {
 		for (var entity of this.entities) {
@@ -42,22 +48,36 @@ class SceneHandler {
 		}
 	}
 }
-interface Entity {
-	tick(): void;
-	getMesh(): THREE.Mesh;
+class Entity {
+	protected mesh: THREE.Object3D;
+	public tick(): void {
+		
+	}
+	public getRenderObject(): THREE.Object3D {
+		return this.mesh;
+	}
 }
-class MyCube implements Entity {
-	private mesh: THREE.Mesh;
+class MyCube extends Entity {
 	constructor() {
+		super();
 		var geometry = new THREE.BoxGeometry(1, 1, 1);
 		var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 		this.mesh = new THREE.Mesh(geometry, material);
-	}
-	public getMesh(): THREE.Mesh {
-		return this.mesh;
 	}
 	public tick() {
 		this.mesh.rotation.x += 0.1;
 		this.mesh.rotation.y += 0.1;
 	}
+}
+class MyLines extends Entity {
+	constructor() {
+		super();
+		var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+		var geometry = new THREE.Geometry();
+		geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
+		geometry.vertices.push(new THREE.Vector3( 0, 10, 0) );
+		geometry.vertices.push(new THREE.Vector3( 10, 0, 0) );
+		this.mesh = new THREE.Line(geometry, material);
+	}
+	
 }
